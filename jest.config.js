d43de15effdee9416d1e5b28f8e979b407f86c53
pcafstockf <github@bytelightning.com>
2021-06-11 13:36:52 -0700

    Comments and cleanup (no code changes).

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
+// Our package.json doesn't list Jest (to keep the install lightweight), but Jest + IntelliJ makes debugging unit tests really easy.
 module.exports = {
 	testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
 	transform: {
